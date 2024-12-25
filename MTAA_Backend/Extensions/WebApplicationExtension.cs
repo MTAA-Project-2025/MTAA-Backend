21f4cd7b38a7d0c928b1a30875c616119d0bf6fc
George Sladkovsky <GeorgiyGitHub@gmail.com>
2024-12-25 17:25:35 +0100

    fix: add web application extension

--- MTAA_Backend/Extensions/WebApplicationExtension.cs
+++ MTAA_Backend/Extensions/WebApplicationExtension.cs
@@ -1,6 +1,27 @@
-﻿namespace MTAA_Backend.Api.Extensions
+﻿using Microsoft.AspNetCore.Localization;
+using MTAA_Backend.Domain.Resources.Localization;
+using System.Globalization;
+
+namespace MTAA_Backend.Api.Extensions
 {
-    public class WebApplicationExtension
+    public static class WebApplicationExtension
     {
+        public static void ConfigureLocalization(this WebApplication app)
+        {
+            var langCodes = Languages.GetAll();
+            var supportedCultures = new List<CultureInfo>(langCodes.Count);
+
+            foreach (var langCode in langCodes)
+            {
+                supportedCultures.Add(new CultureInfo(langCode));
+            }
+
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(Languages.EN),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
+        }
     }
 }
