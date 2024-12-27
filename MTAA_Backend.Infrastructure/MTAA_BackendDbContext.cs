822ad8a806d8164ad0252a54534afa79e233d019
VolodymyrSorochynsky <sorochinskiyuv@gmail.com>
2024-12-27 23:02:13 +0200

    feat: add dbcontext class [MTAA-4]

---
+++ MTAA_Backend.Infrastructure/MTAA_BackendDbContext.cs
@@ -0,0 +1,21 @@
+﻿using MTAA_Backend.Domain.Entities.Users;
+using MTAA_Backend.Domain.Entities.Chats;
+using MTAA_Backend.Domain.Entities.Messages;
+using MTAA_Backend.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace MTAA_Backend.Infrastructure
+{
+    public class MTAA_BackendDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Chat> Chats { get; set; }
+
+        public DbSet<Message> Messages { get; set; }
+
+        public MTAA_BackendDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
