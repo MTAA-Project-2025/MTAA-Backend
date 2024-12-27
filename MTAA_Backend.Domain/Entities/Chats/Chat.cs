59601c8d728f465587b6b9b7710ff8012bddbb6e
VolodymyrSorochynsky <sorochinskiyuv@gmail.com>
2024-12-27 21:18:19 +0200

    feat: add chat entity [MTAA-4]

---
+++ MTAA_Backend.Domain/Entities/Chats/Chat.cs
@@ -0,0 +1,21 @@
+using MTAA_Backend.Domain.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTAA_Backend.Domain.Entities.Chats
+{
+    public class Chat
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Theme { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public List<User> Participants { get; set; } = new List<User>();
+    }
+}
