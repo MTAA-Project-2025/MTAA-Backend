f722b189ab3eb91c7d317b5659b3b50d6a781324
VolodymyrSorochynsky <sorochinskiyuv@gmail.com>
2024-12-27 20:34:33 +0200

    feat: add user entity [MTAA-4]

---
+++ MTAA_Backend.Domain/Entities/Users/User.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTAA_Backend.Domain.Entities.Users
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Username { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime LastSeen { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
