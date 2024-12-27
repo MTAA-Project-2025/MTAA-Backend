07c9de3dd94692c14d3041fa6b07518d5df0126e
VolodymyrSorochynsky <sorochinskiyuv@gmail.com>
2024-12-27 21:27:31 +0200

    feat: add message entity [MTAA-4]

---
+++ MTAA_Backend.Domain/Entities/Messages/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTAA_Backend.Domain.Entities.Messages
+{
+    internal class Message
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid ChatId { get; set; }
+        public Guid SenderId { get; set; }
+
+        public string? Content { get; set; }
+        public string Type { get; set; }
+
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+        public bool IsRead { get; set; } = false;
+    }
+}
