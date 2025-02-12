bd0282aba9750925f12fe72026a5f180b5168bfb
George Sladkovsky <102670724+GeorgiyGit@users.noreply.github.com>
2025-02-13 00:36:54 +0100

    feat: Add Group Join and Leave functionality [MTAA-65]

---
+++ MTAA_Backend.Application/CQRS/Groups/BaseGroups/Events/LeaveGroupEvent.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace MTAA_Backend.Application.CQRS.Groups.BaseGroups.Events
+{
+    public class LeaveGroupEvent : INotification
+    {
+        public Guid GroupId { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ MTAA_Backend/Controllers/Groups/BaseGroupController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using MTAA_Backend.Application.CQRS.Groups.BaseGroups.Commands;
+using MTAA_Backend.Application.CQRS.Groups.Chats.Commands;
+using MTAA_Backend.Domain.DTOs.Groups.Chats.Requests;
+using MTAA_Backend.Domain.DTOs.Shared.Requests;
+using MTAA_Backend.Domain.Interfaces;
+using MTAA_Backend.Domain.Resources.Customers;
+using MTAA_Backend.Domain.Resources.Localization.Errors;
+using MTAA_Backend.Infrastructure;
+using System.Net;
+
+namespace MTAA_Backend.Api.Controllers.Groups
+{
+    public class BaseGroupController : ApiController
+    {
+        private readonly IUserService _userService;
+        public BaseGroupController(IMediator mediator,
+            IMapper mapper,
+            IUserService userService) : base(mediator, mapper)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = UserRoles.User)]
+        [Route("join")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult> JoinGroup([FromBody] GenericIdRequest<Guid> request)
+        {
+            var userId = _userService.GetCurrentUserId();
+            await _mediator.Send(new JoinGroup()
+            {
+                GroupId = request.Id,
+                UserId = userId
+            });
+            return Ok();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = UserRoles.User)]
+        [Route("leave")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult> LeaveGroup([FromBody] GenericIdRequest<Guid> request)
+        {
+            var userId = _userService.GetCurrentUserId();
+            await _mediator.Send(new LeaveGroup()
+            {
+                GroupId = request.Id,
+                UserId = userId
+            });
+            return Ok();
+        }
+    }
+}
