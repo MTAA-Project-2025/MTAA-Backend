4128ffff9339089bfa0b71647c1148228670eefe
VolodymyrSorochynsky <159086929+VolodymyrSorochynsky@users.noreply.github.com>
2025-03-06 14:41:38 +0200

    feat: add UserRelationships class

---
+++ MTAA_Backend.Domain/Entities/Users/UserRelatioships.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTAA_Backend.Domain.Entities.Users
+{
+    public class UserRelatioships
+    {
+        public User User1 { get; set; }
+        public User User2 { get; set; }
+        public bool IsUser1Followig {  get; set; }
+        public bool IsUser2Followig { get; set; }
+    }
+}
