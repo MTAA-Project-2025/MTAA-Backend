c36562f7e01b0dc43f3b8fb027b0da5d4958f08d
Volodymyr <sorochinskiyuv@gmail.com>
2025-04-14 14:15:44 +0300

    feat: add getNotifications query [MTAA-87]

---
+++ MTAA_Backend.Application/CQRS/Notifications/Queries/GetNotifications.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using MTAA_Backend.Domain.DTOs.Notifications.Responses;
+using MTAA_Backend.Domain.DTOs.Shared.Requests;
+
+namespace MTAA_Backend.Application.CQRS.Notifications.Queries
+{
+    public class GetNotifications : IRequest<List<NotificationResponse>>
+    {
+        public PageParameters PageParameters { get; set; }
+    }
+}
---
+++ MTAA_Backend.Application/CQRS/Notifications/QueryHandlers/GetNotificationsHandler.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MTAA_Backend.Application.CQRS.Notifications.Queries;
+using MTAA_Backend.Domain.DTOs.Images.Response;
+using MTAA_Backend.Domain.DTOs.Notifications.Responses;
+using MTAA_Backend.Domain.Interfaces;
+using MTAA_Backend.Domain.Resources.Images;
+using MTAA_Backend.Infrastructure;
+
+namespace MTAA_Backend.Application.CQRS.Notifications.QueryHandlers
+{
+    public class GetNotificationsHandler(MTAA_BackendDbContext _dbContext,
+        IUserService _userService,
+        IMapper _mapper) : IRequestHandler<GetNotifications, List<NotificationResponse>>
+    {
+        public async Task<List<NotificationResponse>> Handle(GetNotifications request, CancellationToken cancellationToken)
+        {
+            var userId = _userService.GetCurrentUserId();
+
+            var notifications = await _dbContext.Notifications
+                .Where(n => n.UserId == userId)
+                .OrderByDescending(n => n.DataCreationTime)
+                .Skip(request.PageParameters.PageNumber * request.PageParameters.PageSize)
+                .Take(request.PageParameters.PageSize)
+                .Include(n => n.Post)
+                    .ThenInclude(p => p.Images)
+                        .ThenInclude(ig => ig.Images)
+                .Include(n => n.Comment)
+                .Select(n => new
+                {
+                    Id = n.Id,
+                    Title = n.Title,
+                    Text = n.Text,
+                    Type = n.Type,
+                    DataCreationTime = n.DataCreationTime,
+                    PostId = n.PostId,
+                    CommentId = n.CommentId,
+                    UserId = n.UserId,
+                    Image = n.Post != null && n.Post.Images.Any() ?
+                        n.Post.Images.First().Images.Where(i => i.Type == ImageSizeType.Small).FirstOrDefault() : null
+                })
+                .ToListAsync(cancellationToken);
+
+            var mappedNotifications = new List<NotificationResponse>(notifications.Count);
+
+            foreach (var notification in notifications)
+            {
+                mappedNotifications.Add(new NotificationResponse()
+                {
+                    Id = notification.Id,
+                    Title = notification.Title,
+                    Text = notification.Text,
+                    Type = notification.Type,
+                    DataCreationTime = notification.DataCreationTime,
+                    PostId = notification.PostId,
+                    CommentId = notification.CommentId,
+                    UserId = notification.UserId,
+                    Image = notification.Image != null ?
+                        _mapper.Map<MyImageResponse>(notification.Image) : null
+                });
+            }
+
+            return mappedNotifications;
+        }
+    }
+}
