4a5d665537f160a7f29db878ff92d44313873ab5
Volodymyr <sorochinskiyuv@gmail.com>
2025-04-14 13:23:42 +0300

    feat: add AddNotification command [MTAA-87]

---
+++ MTAA_Backend.Application/CQRS/Notifications/CommandHandlers/AddNotificationHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using MTAA_Backend.Application.CQRS.Notifications.Commands;
+using MTAA_Backend.Domain.Entities.Notifications;
+using MTAA_Backend.Infrastructure;
+
+namespace MTAA_Backend.Application.CQRS.Notifications.CommandHandlers
+{
+    public class AddNotificationHandler(MTAA_BackendDbContext _dbContext,
+        IMediator _mediator) : IRequestHandler<AddNotification>
+    {
+        public async Task Handle(AddNotification request, CancellationToken cancellationToken)
+        {
+            var notification = new Notification
+            {
+                Title = request.Title,
+                Text = request.Text,
+                Type = request.Type,
+                UserId = request.UserId,
+                PostId = request.PostId,
+                CommentId = request.CommentId
+            };
+
+            await _dbContext.Notifications.AddAsync(notification, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            /*
+            await _mediator.Publish(new NotificationCreatedEvent
+            {
+                NotificationId = notification.Id,
+                UserId = notification.UserId
+            });
+            */
+        }
+    }
+}
